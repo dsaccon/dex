848c69fbffb656ad3797e167b1910df1a90918da
ds <deerff@gmail.com>
2020-12-12 16:22:49 -0800

    clean up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,6 @@
 var Dex = artifacts.require("Dex");
 var Token = artifacts.require("Token");
 
-/*module.exports = async function (deployer) {
-	deployer.deploy(Token, "token", "TKN", 18, 1000000).then(function() {
-  		return deployer.deploy(Dex, Token.address);
-	});
-	let dex_inst = await Dex.deployed()
-	dex_inst.init(50, {value: 50*10**18})
-};*/
-
-//module.exports = async function (deployer) {
-//	deployer.deploy(Token, "token", "TKN", 18, 1000000)
-//		.then(() => Token.deployed())
-//		.then(() => deployer.deploy(Dex, Token.address))
-//		.then(() => Dex.deployed());
-//
-//		.then(() => let dex_inst = await Dex.deployed())
-//		.then(() => dex_inst.init(50, {value: 50*10**18}));
-//	let dex_inst = await Dex.deployed()
-//	dex_inst.init(50, {value: 50*10**18})
-//};
-
 module.exports = async function (deployer) {
 	deployer.deploy(Token, "token", "TKN", 18, 1000000)
 		.then(() => Token.deployed())
@@ -30,9 +10,3 @@ module.exports = async function (deployer) {
 			Dex_instance.init(50, {value: 50*10**18})
 		});
 };
-		
-//		.then(() => let dex_inst = await Dex.deployed())
-//		.then(() => dex_inst.init(50, {value: 50*10**18}));
-//	let dex_inst = await Dex.deployed()
-//	dex_inst.init(50, {value: 50*10**18})
-//};
