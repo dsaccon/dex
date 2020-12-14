35d17fec49d2af8e5c04721b2bbe813a31ae5a57
ds <deerff@gmail.com>
2020-12-13 18:02:29 -0800

    add rinkeby network config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,13 @@
 const path = require("path");
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "d16c40e6830949fd9bbc5eb498fca4de";
+
+const fs = require('fs');
+const mnemonic = 'draft broccoli cute stairs aisle garden under palm calm minimum name plunge'
+
+// https://rinkeby.infura.io/v3/d16c40e6830949fd9bbc5eb498fca4de
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,6 +17,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
-  }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + infuraKey),
+      network_id: 4,
+      gas: 5500000,
+    },
+  },
 };
